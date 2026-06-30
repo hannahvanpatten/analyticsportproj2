fc7b84d3a93ae888a44b9d2e202898976499e16f
Hannah <hnvanpatten@gmail.com>
2026-06-30 13:57:59 -0700

    Create baseline_models_fixed.py

---
+++ baseline_models_fixed.py
@@ -0,0 +1,15 @@
+# ----------------Imports----------------
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+df = pd.read_csv("dataset_with_derived_metrics.csv", parse_dates=["date"])
+
+
+# ---------Define Baseline Models--------
+
+# ------------Function Calls-------------
+
+# -------------Save Outputs--------------
+
+# ---------------Results-----------------
